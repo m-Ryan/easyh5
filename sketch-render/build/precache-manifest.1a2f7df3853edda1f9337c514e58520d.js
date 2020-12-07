a9ed1411358a3f485285e6965263bd3cf3b7add2
ch.mao <ch.mao@chmao-Mac.local>
2020-12-07 17:30:01 +0800

    :package: Updating compiled files

---
+++ sketch-render/build/precache-manifest.1a2f7df3853edda1f9337c514e58520d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2931bf5aeeaa3c4fd333b71ed6a1b001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3532bc5cc8ad7d852da7",
+    "url": "/static/css/main.76b2af3f.chunk.css"
+  },
+  {
+    "revision": "937a9bfb9cec7be3d9d5",
+    "url": "/static/js/2.fb55982a.chunk.js"
+  },
+  {
+    "revision": "3532bc5cc8ad7d852da7",
+    "url": "/static/js/main.5972d25e.chunk.js"
+  },
+  {
+    "revision": "9c09a65c29d8a1eb2c5f",
+    "url": "/static/js/runtime-main.7203baa5.js"
+  }
+]);
