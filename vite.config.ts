e98fba81d16c4774e0b3d767cddaa4b2da8b64fd
maocanhua <962491243@qq.com>
2021-02-21 13:21:34 +0800

    vite

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@VisualEditor': path.resolve('src/components/VisualEditor'),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+
+    // commonjsOptions: { include: 'node_modules/**' }
+  },
+  optimizeDeps: {
+    // include: ['node_modules/insert-css/index.js']
+  },
+  css: {
+    modules: {
+      localsConvention: 'dashes',
+    },
+    preprocessorOptions: {
+      scss: {
+
+      }
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://h5.maocanhua.cn',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
+  },
+  plugins: [reactRefresh()]
+});
